dd401b3582000c701d3a67dff50da9d9fc2a08d2
Jakabos Lorand <jakaboslorand@uni.sapientia.ro>
2025-10-27 19:35:47 +0200

    HF3 masodik feladat kesz

--- HF3/TableConstraintLayout/app/src/main/java/com/example/tableconstraintlayout/MainActivity.java
+++ HF3/TableConstraintLayout/app/src/main/java/com/example/tableconstraintlayout/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tableconstraintlayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etCode, etName, etPrice;
+    private Button btnAdd, btnCancel, btnShow;
+    private TextView tvProducts;
+
+    private ArrayList<Product> prodList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,56 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        etCode = findViewById(R.id.productCode);
+        etName = findViewById(R.id.productName);
+        etPrice = findViewById(R.id.productPrice);
+        btnAdd = findViewById(R.id.add);
+        btnCancel = findViewById(R.id.cancel);
+        btnShow = findViewById(R.id.showButton);
+        tvProducts = findViewById(R.id.productList);
+
         setTitle("TableLayout2");
+
+        btnAdd.setOnClickListener(v -> {
+            String code = etCode.getText().toString();
+            String name = etName.getText().toString();
+            String priceStr = etPrice.getText().toString();
+
+            if (code.isEmpty() || name.isEmpty() || priceStr.isEmpty()) {
+                Toast.makeText(this, "Adj meg minden információt!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            try {
+                double price = Double.parseDouble(priceStr);
+
+                Product prod = new Product(code, name, price);
+                prodList.add(prod);
+
+                Toast.makeText(this, "Termék hozzáadva.", Toast.LENGTH_SHORT).show();
+            } catch (NumberFormatException e) {
+                Toast.makeText(this, "Számot adj meg árként!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        btnCancel.setOnClickListener(v -> {
+            etCode.setText("");
+            etName.setText("");
+            etPrice.setText("");
+        });
+
+        btnShow.setOnClickListener(v -> {
+            if (prodList.isEmpty()) {
+                tvProducts.setText("Üres a lista.");
+                return;
+            }
+
+            StringBuilder list = new StringBuilder();
+            for (Product prod : prodList) {
+                list.append(prod.toString()).append("\n");
+            }
+            tvProducts.setText(list.toString());
+        });
     }
 }
